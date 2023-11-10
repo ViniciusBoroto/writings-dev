1f7fcafc32f820b4a4039c5a6125b6b540edb217
Vinícius Boroto dos Anjos <viniciusdosanjos63@gmail.com>
2023-11-09 21:30:32 -0300

    hamburger menu start

---
+++ script.js
@@ -0,0 +1,13 @@
+const navList = document.getElementById('nav-list')
+
+function hamburgerMenuClick() {
+    if (navList.style.display == 'none') {
+        navList.style.display = 'block'
+    }   else {
+        navList.style.display = 'none'
+        }
+}
+
+// function navBarChange() {
+
+// }
